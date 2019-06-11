7f1dbcc77e0984f1497780d1f278fd4cd151c4ee
company_gabriel <Gabriel_wei@163.com>
2019-06-11 18:35:26 +0800

    调整eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,16 @@
 module.exports = {
-	'env': {//你的脚本将要运行在什么环境中   Environment可以预设好的其他环境的全局变量，如brower、node环境变量、es6环境变量、mocha环境变量等
-		'browser': true,//浏览器
-		'es6': true,//支持es6
-		'node': true//node
+	root: true,
+	env: {
+		node: true
 	},
-	'extends': [
-		'eslint:recommended',
-		'plugin:vue/essential'
-	],
-	'globals': { //额外的全局变量
-		'Atomics': 'readonly',
-		'SharedArrayBuffer': 'readonly'
+	extends: ['plugin:vue/essential', 'eslint:recommended'],
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'vue/no-parsing-error': [2, { 'x-invalid-end-tag': false }],
+		'no-tabs': 'off'
 	},
-	'parserOptions': {
-		'ecmaVersion': 2018,
-		'sourceType': 'module'
-	},
-	'plugins': [
-		'vue'
-	],
-	'rules': {
-		'indent': [
-			'error',
-			'tab'
-		],
-		'linebreak-style': [
-			'error',
-			'windows'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'always'
-		]
+	parserOptions: {
+		parser: 'babel-eslint'
 	}
 };
